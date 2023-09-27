c2225ec7bf71d4bbb3bc24d6260f5de3f466512c
Olamilekan <ayomikunolamilekan@gmail.com>
2023-09-27 15:31:40 +0100

    update

--- src/header.jsx
+++ src/header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Header() {
     return (
         <div className="flex justify-between p-5 bg-[#D9D9D9] w-full h-16 sticky top-0 z-10">
-            <a href="#home"><img src="public/images/eprenz.svg" className="w-24 -mt-1" /></a>
+            <a href="#home"><img src="/images/eprenz.svg" className="w-24 -mt-1" /></a>
             <p className="text-xl"><a href="#coach-track">COACH TRACK</a></p>
             <p className="text-base"><a href="#pricing">PRICING</a></p>
         </div>
